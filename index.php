8ec942543bd116c7502c8517c083a6ba9206059f
anas-ero <anassanda111@gmail.com>
2025-11-24 10:49:02 +0100

    Final version

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
+<?php 
+
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'todolist');
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+};
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    if (isset($_POST['action'])) {
+        $action = $_POST['action'];
+
+        if ($action === "new") {
+            $title = $conn->$_POST['title'];
+            if (!empty($title)) {
+                $conn->query("INSERT INTO todolist (title) VALUES ('$title')");
+            }
+        }
+
+        if ($action === "delete") {
+            $id = $_POST["id"];
+            $conn->query("DELETE FROM todolist WHERE id= $id");
+        }
+
+        if ($action === "toggle") {
+            $id = $_POST["id"];
+            $conn->query("UPDATE todolist SET done = 1 - done WHERE id = $id");
+        }
+    }
+}
+
+$tasks = [];
+$result = $conn->query("SELECT * FROM todolist order by created_at DESC");
+
+
+while ($row = $result->fetch_assoc()) {
+    $taches[] = $row;
+}
+
+?>
+
+ 
+    
+
+
+
+
+
+ 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,7 +78,24 @@
 
         
             <ul class="list-group mt-4">
-                <?php foreach
+                <?php foreach ($tasks as $t) ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <div>
+                            <form method="post" class="d-inline">
+                                <input type="hidden" name="id" value="<?php echo $t['id']; ?>">
+                                <button class="btn btn-link p-0 m-0 align-baseline" value="toggle" name="action">
+                                    <?php if ($t['done']) { ?>
+                                        <?php echo htmlspecialchars($t['title']); ?>
+                                    <?php } else { ?>
+                                        <?php echo htmlspecialchars($t['title']); ?>
+                                    <?php } ?>
+                                </button>
+                            </form>
+                        </div>
+                        <form method="post">
+                            <input type="hidden" name="id" value="<?php echo $t['id']; ?>">
+                            <button class="btn btn-danger btn-sm" value="delete" name="action">Delete</button>
+                        </form>
                 
                 
                 
