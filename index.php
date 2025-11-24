20807f121df5e914c62e6f2b012205996dfcbf7e
anas-ero <anassanda111@gmail.com>
2025-11-24 10:45:50 +0100

    inti version

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body class="bg-light row">
+    <form class="container" method="post">
+        <div class="bg-dark py-3 ">
+            <h1 class="text-center text-white">To-Do List</h1>
+        </div>
+
+        <div class="d-flex justify-content-center mt-4">
+            <input type="text" placeholder="Task Title.." class="form-control mt-4" name="title">
+            <button class=" btn btn-primary mt-4" value="new" name="action">Add</button>
+        </div>
+
+        
+            <ul class="list-group mt-4">
+                <?php foreach
+                
+                
+                
+                
+                ?>
+            </ul>
+    </form>
+    
+    
+    
+</body>
+</html>
